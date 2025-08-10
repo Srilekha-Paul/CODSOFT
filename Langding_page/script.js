7fb3a41e0c2cb559aba124a866f80705230ecc1f
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-08-10 16:31:41 +0530

    add js and html

--- Langding_page/script.js
+++ Langding_page/script.js
@@ -1,10 +1,17 @@
 const menuBTN = document.getElementById("menu-btn");
 
-const navlinks = ocument.getElementById("nav_links");
+const navlinks = document.getElementById("nav_links");
 
-const menuBTNicon = menuBTN.querySelector("i");
+const menuBTNIcon = menuBTN.querySelector("i");
+
 menuBTN.addEventListener("click", (e) =>{
+
     navlinks.classList.toggle("open");
 
+    const isOpen = navlinks.classList.contains("open");
+
+    menuBTNIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-fill");
+
+
     
-})
+});
