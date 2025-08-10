7be2650fe446dc64d17b27c5e95f6e2b913a8ca1
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-08-10 17:05:52 +0530

    add first part js

--- Langding_page/script.js
+++ Langding_page/script.js
@@ -1,12 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBTN = document.getElementById("menu-btn");
     const navlinks = document.getElementById("nav_links");
-    const menuBTNIcon = menuBTN.querySelector("i");
-
-    menuBTN.addEventListener("click", () => {
-        navlinks.classList.toggle("open");
-
-        const isOpen = navlinks.classList.contains("open");
-        menuBTNIcon.className = isOpen ? "ri-close-line" : "ri-menu-fill";
-    });
+    const icon = menuBTN.querySelector("i");
 });
