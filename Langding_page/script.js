3d16f67597e6605bbcc7bab0899d094023965eb1
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-08-10 17:11:01 +0530

    complete js

--- Langding_page/script.js
+++ Langding_page/script.js
@@ -2,4 +2,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuBTN = document.getElementById("menu-btn");
     const navlinks = document.getElementById("nav_links");
     const icon = menuBTN.querySelector("i");
+
+    menuBTN.addEventListener("click",() =>{
+        navlinks.classList.toggle("open");
+        icon.className = navlinks.classList.contains("open") ? "ri-close-line" : "ri-menu-fill";
+    });
+    
+    
 });
